e908e1522fd9514196af3fb29465f9d227d2d2f4
Raphael Hemme <hemme.raphael@gmail.com>
2020-11-14 11:37:48 +0100

    Build basic fighting logic with console.log only

---
+++ src/BattleCards.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import PokeProperties from './PokeProperties';
+
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function BattleCards(props) {
+  const classes = useStyles();
+  // const handleSelection = props.handleSelection
+  // const handleEnemy = props.handleEnemy
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+    
+        <CardMedia
+          className={classes.media}
+          image={props.pokemon.image}
+          title={props.pokemon.name.english}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.pokemon.name.english}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Properties
+            <PokeProperties pokemon={props.pokemon} opponent={props.opponent}/>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+
+//  <PokeProperties pokemon={props.pokemon} opponent={props.opponent}/>
---
+++ src/PokeProperties.js
@@ -0,0 +1,58 @@
+import React from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+function generate(element) {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    })
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 752,
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+  },
+}));
+
+const PokeProperties = (props) => {
+  const classes = useStyles();
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={6}>
+        <Typography variant="h6" className={classes.title}>
+          Properties
+        </Typography>
+        <div className={classes.demo}>
+          <List dense={dense}>
+            {generate(
+              <ListItem>
+                <ListItemText
+                  primary="Single-line item"
+                  secondary={secondary ? "Secondary text" : null}
+                />
+              </ListItem>
+            )}
+          </List>
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default PokeProperties;
